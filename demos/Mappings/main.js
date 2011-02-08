a40317640092997d3aca5b80a70a9ba2bf1ac8c1
cadorn <christoph@christophdorn.com>
2011-02-08 10:27:51 -0800

    Changes to mappings demo to be able to run on server

---
+++ demos/Mappings/main.js
@@ -0,0 +1,42 @@
+
+module.declare(["package1/main", "package4/main"], function(require, exports, module)
+{
+    exports.main = function()
+    {
+        // Program is booted with all required depdnencies loaded. We can now initialize and execute.
+        
+        // NOTE: This assumes intimate knowledge about the location of package1 which is fine
+        //       since the package we are calling is somewhat special in that is requires packages
+        //       as well as modules based on the mainModuleDir.
+        //       To make this absolutely generic the package being called should bundle all it's
+        //       modules into a package.
+        var mainModuleDir = module.mappings["package1"].split("/");
+        mainModuleDir.pop();
+        mainModuleDir.pop();
+
+        var options = {
+            mainModuleDir: mainModuleDir.join("/") + "/"
+        };
+
+        // Require a package's main module simply by using the label provided to the dependency array
+        var package1 = require("package1/main");
+        package1.main(options);
+        module.print(package1.getMessages() + "\n");
+    
+        var package2 = require("package4/main");
+        package2.main(options);
+        module.print(package2.getMessages() + "\n");
+        
+        // The function we are calling lazy-loads extra code into the running program - hence the callback
+        package2.loadSubModule(function(subModule) {
+            subModule.main(options);
+            module.print(subModule.getMessages() + "\n");
+        });
+    
+        // The function we are calling lazy-loads extra code into the running program - hence the callback
+        package2.loadSubPackage(function(package3) {
+            package3.main(options);
+            module.print(package3.getMessages() + "\n");
+        });
+    }
+});
